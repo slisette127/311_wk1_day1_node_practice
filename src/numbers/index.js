eeedfc7683f8cb03e3bb910694e02e3c789b8e72
slisette127 <slisette127@gmail.com>
2021-07-05 20:14:34 -0500

    js

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,19 +1,28 @@
 const isEven = (num) => {
   // write code for numbers.isEven
   if (num % 2 == 0){
-  return True}
-  else {
-  return False}
+  return true
+  }
 }
 
 const sum = (arr) => {
   // write code for numbers.sum
-
+  const newVar = arr.reduce(function(accumulator, current){
+    return accumulator + current;
+  }
+  )
+  return newVar;
 }
+
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
-
+  for (let i=0; i > arr.length; i++){
+    for (let j=1; j > arr.length-1; j++){
+      if (arr[i]+ arr[j] === sum)
+    }
+  }
+  
 }
 
 module.exports = {
